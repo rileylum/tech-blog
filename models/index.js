702cde404ee25ba76e50f35741c08aead7948c00
Riley Lum <rileylum@adam.com.au>
2021-09-07 17:34:51 +0930

    define model relationships

---
+++ models/index.js
@@ -0,0 +1,24 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// One User has Many Posts
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+// One Post has One User
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+// One Post has Many Comments
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+// One Comment has One Post
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
