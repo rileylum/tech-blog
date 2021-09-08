90a361523804055039f6cfbac9a9eae8c3341dd0
Riley Lum <rileylum@adam.com.au>
2021-09-08 15:06:55 +0930

    add login and sign up routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -5,15 +5,15 @@ const { User } = require('../../models');
 router.post('/signup', async (req, res) => {
     try {
         const userData = await User.create(req.body);
-
+        const user = userData.get({plain:true});
+        console.log(user);
         req.session.save(() => {
             req.session.user_id = userData.id;
             req.session.logged_in = true;
-
-            res.status(200).json(userData);
+            res.status(200).json(user);
         });
     } catch (err) {
-        res.status(400).json(err)
+        res.status(400).json(err.stack)
     }
 });
 
